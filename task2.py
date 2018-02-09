ffbef435705301c6f632dd201222ca72cb61ead3
heydii <piotr.hejduk@gmail.com>
2018-02-09 18:38:39 +0100

    Dodano plik z planem zajęć

--- task2.py
+++ task2.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 dziennik = {"Kowalski" : ['2', '3', '2'], "Nowak" : ['4', '5', '4'], "Maliniak" : ['1', '3', '1']}
 nazwisko = input("Podaj nazwisko ucznia: ")
-if nazwisko == "Kowalski":
-    print('\n'.join(dziennik.get(nazwisko)))
-elif nazwisko == "Nowak":
-    print('\n'.join(dziennik.get(nazwisko)))
-elif nazwisko == "Maliniak":
+if nazwisko in dziennik:
     print('\n'.join(dziennik.get(nazwisko)))
 else:
     exit()
